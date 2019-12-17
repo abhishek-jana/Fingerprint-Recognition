75840579c6894d7e3c7ef9a951eaf05059ce69c7
Abhishek Jana <48491625+abhishek-jana@users.noreply.github.com>
2019-12-17 03:44:24 -0600

    Add files via upload
    Read Data Files

---
+++ utils.py
@@ -0,0 +1,25 @@
+import numpy as np
+import os
+
+
+def get_data(path):
+    data = {}
+    for files in os.listdir(path):
+        keys,_ = files.split('_')
+        if keys in data:
+            data[keys].append(files)
+        else:
+            data[keys] = [files]
+    return data
+
+def train_test_split(data, ratio = 0.2):
+    train = {}
+    test = {}
+    for key in data.keys():
+        vals = data[key]
+        split = int(len(vals)*ratio)
+        train[key] = vals[split:]
+        test[key] = vals[:split]
+    return train,test
+
+
